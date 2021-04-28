0995abfbec2fe8a529ac3162c30ef6147028bbf5
Ilya Artamonov <ilya.sosidka@gmail.com>
2021-04-28 17:32:36 +0300

    Fix classification

--- dist/VueFormulateDatepicker.common.js
+++ dist/VueFormulateDatepicker.common.js
@@ -264,7 +264,7 @@ if (typeof window !== 'undefined') {
 // Indicate to webpack that this file can be concatenated
 /* harmony default export */ var setPublicPath = (null);
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"7a52abac-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/VueFormulateDatepicker.vue?vue&type=template&id=0b2ee1f4&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"06b18b9a-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/VueFormulateDatepicker.vue?vue&type=template&id=0b2ee1f4&
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('Datepicker',_vm._b({class:("formulate-input-element formulate-input-element--" + (_vm.context.type)),attrs:{"data-type":_vm.context.type,"options":_vm.context.options,"language":_vm.languages[_vm.context.attributes.language]},model:{value:(_vm.context.model),callback:function ($$v) {_vm.$set(_vm.context, "model", $$v)},expression:"context.model"}},'Datepicker',_vm.context.attributes,false))}
 var staticRenderFns = []
 
@@ -4354,7 +4354,7 @@ function VueFormulateDatepickerPlugin(formulateInstance) {
     },
     library: {
       datepicker: {
-        classification: "datepicker",
+        classification: "date",
         component: "VueFormulateDatepicker"
       }
     }
--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ export default function VueFormulateDatepickerPlugin(formulateInstance) {
     },
     library: {
       datepicker: {
-        classification: "datepicker",
+        classification: "date",
         component: "VueFormulateDatepicker",
       },
     },
